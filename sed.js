5f1b09e49fc644f34e1fde77c4da97368eaa7f3a
hari <hari.k.krishna4@gmail.com>
2019-01-27 18:26:22 -0700

    sed.js

--- sed.js
+++ sed.js
@@ -1,6 +1,16 @@
+// This is your Editor pane. Write your JavaScript here and 
+// use the command line to execute commands
 var shell = require('shelljs');
 
 if (!shell.which('git')) {
     shell.echo('Sorry, this script requires git');
     shell.exit(1);
 }
+
+
+// Replace macros in each .js file
+shell.cd('./files');
+shell.ls('*.js').forEach(function (file) {
+    shell.sed('-i', /^.*node.*$/, '', file);
+    shell.sed('-i', /.*express.*\n/, shell.cat('text.js'), file);
+});
